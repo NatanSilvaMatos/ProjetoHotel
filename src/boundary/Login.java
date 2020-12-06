2d5d3bef374a6a6685ca097fa8350f0c47a77a75
Natan <Natan@DESKTOP-BF3LPKV>
2020-12-05 21:50:41 -0300

    Trabalho Semestral da Matéria de Programação Orientado a Objetos

--- src/boundary/Login.java
+++ src/boundary/Login.java
@@ -1,10 +1,8 @@
 package boundary;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 
+import java.io.InputStream;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
--- src/control/ControlTeste.java
+++ src/control/ControlTeste.java
@@ -1,5 +1,7 @@
 package control;
 
 public class ControlTeste {
-
+	public static void main(String[] args) {
+		System.out.println("cacete");
+	}
 }
