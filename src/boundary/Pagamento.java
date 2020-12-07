c40d36c2a3b1f8069b3318823f0381b09ace0380
Natan <Natan@DESKTOP-BF3LPKV>
2020-12-07 01:52:49 -0300

    Telas

--- src/boundary/Pagamento.java
+++ src/boundary/Pagamento.java
@@ -1,15 +1,19 @@
 package boundary;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Pagamento{
+public class Pagamento implements EventHandler<ActionEvent>{
 	private Pane pane = new Pane();
 
 	private Label lblCPF = new Label("CPF HOSPEDE:");
@@ -27,7 +31,9 @@ public class Pagamento{
 	private RadioButton rbCredito = new RadioButton("Credito");
 	private RadioButton rbDinheiro = new RadioButton("Dinheiro");
 	private Button confirmarPagamento = new Button("Confirmar Pagamento");
+	private Button pesquisarCpf = new Button("Pesquisar");
 	private ToggleGroup groupTipoPagamento = new ToggleGroup();
+	private Alert alert = new Alert(AlertType.WARNING);
 		
 	public Pagamento() {	
 		lblPagamento.setLayoutX(50);
@@ -39,6 +45,9 @@ public class Pagamento{
 		
 		txtCPF.setLayoutX(50);
 		txtCPF.setLayoutY(120);
+		
+		pesquisarCpf.setLayoutX(220);
+		pesquisarCpf.setLayoutY(120);
 		
 		lblCartao.setLayoutX(50);
 		lblCartao.setLayoutY(190);
@@ -72,6 +81,7 @@ public class Pagamento{
 		rbDebito.setToggleGroup(groupTipoPagamento);
 		rbDebito.setSelected(true);
 		
+		
 		rbCredito.setLayoutX(500);
 		rbCredito.setToggleGroup(groupTipoPagamento);
 		rbCredito.setLayoutY(180);
@@ -79,17 +89,46 @@ public class Pagamento{
 		rbDinheiro.setLayoutX(500);
 		rbDinheiro.setToggleGroup(groupTipoPagamento);
 		rbDinheiro.setLayoutY(220);
+		rbDinheiro.setOnAction(this);
 		
 		confirmarPagamento.setLayoutX(300);
 		confirmarPagamento.setLayoutY(500);
 		confirmarPagamento.setPrefWidth(170);
+		confirmarPagamento.setOnAction(this);
 		
-		pane.getChildren().addAll(lblPagamento,lblCPF,txtCPF,lblCartao,txtCartao,lblCodSeguranca,txtCodSeguranca, lblPrecoTotal,lblCedula,txtPrecoTotal,lblTipoPagamento,
-		rbDebito,rbCredito,rbDinheiro, confirmarPagamento);
+		alert.setTitle("Erro");
+		
+		pane.getChildren().addAll(lblPagamento,lblCPF,txtCPF,pesquisarCpf,lblCartao,txtCartao,lblCodSeguranca,txtCodSeguranca, lblPrecoTotal,lblCedula,txtPrecoTotal,
+		lblTipoPagamento,rbDebito,rbCredito,rbDinheiro, confirmarPagamento);
 		
 	}
 
 	public Pane getPane() {
 		return pane;
 	}
+
+	@Override
+	public void handle(ActionEvent e) {
+		if(e.getTarget() == confirmarPagamento) {
+			if(txtCPF.getText().isEmpty()) {
+				alert.setHeaderText("O campo de CPF não foi preenchido");
+				alert.setContentText("Preencha o campo de CPF para prosseguir");
+				alert.showAndWait();
+			}
+		}	
+		if(e.getTarget() == rbDinheiro) {
+			txtCartao.clear();
+			txtCartao.setEditable(false);
+			txtCodSeguranca.clear();
+			txtCodSeguranca.setEditable(false);
+		}
+		if(e.getTarget() == rbCredito || e.getTarget() == rbDebito) {
+			txtCartao.setEditable(true);
+			txtCodSeguranca.setEditable(true);
+		}
+		
+		
+	}
+	
+	
 }
